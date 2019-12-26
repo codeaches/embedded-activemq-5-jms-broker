f0dc8ade68ef655943f4ac128433b0d836d2a227
codeaches <info001@codeaches.com>
2019-12-25 19:05:14 -0700

    Spring Boot JMS embedded broker Example [http://codeaches.com/]

--- src/main/java/com/codeaches/activmq/embedded/EmbeddedActivemqJmsBrokerApplication.java
+++ src/main/java/com/codeaches/activmq/embedded/EmbeddedActivemqJmsBrokerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmbeddedJmsBrokerApplication {
+public class EmbeddedActivemqJmsBrokerApplication {
 
     public static void main(String[] args) {
-	SpringApplication.run(EmbeddedJmsBrokerApplication.class, args);
+	SpringApplication.run(EmbeddedActivemqJmsBrokerApplication.class, args);
     }
 }
